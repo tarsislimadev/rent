588ede0195fc23523dbe474a9080f93fd1e4e912
Tarsis de Lima <br.tmvdl@gmail.com>
2022-10-13 19:29:15 -0300

    Removing environment files

--- env/production/down.sh
+++
@@ -1,5 +0,0 @@
-sudo docker-compose -f docker-compose.yaml down --remove-orphans --rmi all 
-
-sudo rm -rf ./data
-
-sudo rm -rf ./logs
--- env/production/up.sh
+++
@@ -1,1 +0,0 @@
-sudo docker-compose -f docker-compose.yaml up -d --remove-orphans --force-recreate --build 
