470b17cd3b3f04067c71ce1aab059796c97190bc
Tarsis de Lima <br.tmvdl@gmail.com>
2022-10-13 21:07:04 -0300

    Fixing script for PULLING in development environment

--- env/development/pull.sh
+++ env/development/pull.sh
@@ -1,3 +1,5 @@
-git clone git@github.com:brtmvdl/imobiliario-front.git ./src/front
+mkdir -p src/front src/back
 
-git clone git@github.com:brtmvdl/imobiliario-back.git ./src/back
+git clone git@github.com:brtmvdl/imobiliario-front.git src/front
+
+git clone git@github.com:brtmvdl/imobiliario-back.git src/back
