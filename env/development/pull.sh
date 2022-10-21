c5048c59988ee54f5be310a174395855e28a7d5f
Tarsis de Lima <br.tmvdl@gmail.com>
2022-10-21 12:22:46 -0300

    Fixing PULL script

--- env/development/pull.sh
+++ env/development/pull.sh
@@ -1,1 +1,3 @@
-git clone git@github.com:brtmvdl/imobiliario-front.git src/front
+git clone git@github.com:brtmvdl/imobiliario-back.git ./src/back
+
+git clone git@github.com:brtmvdl/imobiliario-front.git ./src/front
