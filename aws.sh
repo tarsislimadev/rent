841ccdcf0f59c1b7b977d8d7ef2aa380e46bde4c
Tarsis de Lima <br.tmvdl@gmail.com>
2022-11-26 04:45:48 -0300

    Fixing GitHub action workflow file

--- aws.sh
+++ aws.sh
@@ -2,6 +2,6 @@ docker run --rm -it \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
-  -v $(pwd):/app
-  -w /app
+  -v $(pwd):/app \
+  -w /app \
   amazon/aws-cli $@
