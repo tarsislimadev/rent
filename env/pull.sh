cf12258e700656dfc4feda19ef09446021d57f37
Tarsis de Lima <br.tmvdl@gmail.com>
2022-11-28 17:01:40 -0300

    Fixing GIT PULL script

--- env/pull.sh
+++ env/pull.sh
@@ -1,3 +1,7 @@
-git clone git@github.com:brtmvdl/imobiliario-back.git ./src/back
+git clone git@github.com:brtmvdl/imobiliario-adslist.git ./src/adslist
 
-git clone git@github.com:brtmvdl/imobiliario-front.git ./src/front
+git clone git@github.com:brtmvdl/imobiliario-api.git ./src/api
+
+git clone git@github.com:brtmvdl/main-node-file.git ./src/main
+
+git clone git@github.com:brtmvdl/imobiliario-front.git ./src/www
